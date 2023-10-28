251fbfc892d01800c5b7c00e29be1ed461b4fd74
Anuj Srivastava <anujs344@gmail.com>
2023-10-28 11:42:07 +0530

    commit 1.0

--- guessTheNumber.cpp
+++ guessTheNumber.cpp
@@ -6,16 +6,20 @@ int guessTheNumber(int currentNumber){
         int newNumber;
         cout <<" Please guess the Number : ";
         cin >> newNumber;
-        cout<<endl;
+        // cout<<endl;
         if(currentNumber == newNumber){
             cout<< " Hurray ! You have Entered the Correct Number "<<endl;
+            break;
         }else if(newNumber > currentNumber){
-            cout<< " Hurray ! You have Entered the Correct Number "<<endl;
+            cout<< "You have entered number higher than the correct Number"<<endl;
         }else{
-            cout<< " Hurray ! You have Entered the Correct Number "<<endl;
+            cout<< "You have entered number lower than the correct Number"<<endl;
         }
     }
 }
 int main(){
+    int currentNumber = rand()%100;
+    guessTheNumber(currentNumber);
+    return 0;
 
 }
